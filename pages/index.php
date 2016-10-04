d518bde58ef8d87e21e3093ac0255cd7ba9dbddc
antoine_weemaels <antoineweemaels@hotmail.com>
2016-10-04 17:44:32 +0200

    Modification Index 2

--- pages/index.php
+++ pages/index.php
@@ -12,17 +12,23 @@
                 <p>Achat de 1276 pièces au prix moyen de 17,35 euros</p>
                 <p>Vente de 834 pièces au pris moyen de 38,13 euros</p>
             </section>
-            
+
             <section>
                 <h2>Ventes des produits d'ameublement</h2>
                 <p>Achat de 816 pièces au prix moyen de 83,45 euros</p>
                 <p>Vente de 609 pièces au prix moyen de 138,71 euros</p>
             </section>
-            
+
             <section>
                 <h2>Contact Responsable des stocks</h2>
                 <p>Juliette : extension 18</p>      
             </section>
+
+            <section>
+                <h2>News</h2>
+                <p>Le site progresse bien</p>
+            </section>
+
         </div>
     </body>
 </html>
